48ae5df83dba9f171ae5d2fc20efc202972c16a3
Lucas20santos <lucas210souza@gmail.com>
2025-11-02 20:46:17 -0300

    quase terminado o exercicio

--- solucoes/atualizacao_inventario.js
+++ solucoes/atualizacao_inventario.js
@@ -1,5 +1,27 @@
-function updateInventory(arr1, arr2) {
-    return arr1;
+function updateInventory(arr1, arr2) 
+{
+    let newInv = []
+
+    for(let i = 0; i < arr2.length; i++)
+    {
+        var naoExiste = true;
+
+        for (let j = 0; j < arr1.length; j++)
+        {
+            if(arr2[i][1] === arr1[j][1])
+            {
+                arr1[j][0] += arr2[i][0];
+                naoExiste = false;
+            }
+        }
+
+        if (naoExiste)
+        {
+            newInv.push(arr2[i]);
+            naoExiste = false;
+        }
+    }
+    return newInv;
 }
 
 // Example inventory lists
@@ -17,4 +39,19 @@ var newInv = [
     [7, "Toothpaste"]
 ];
 
-updateInventory(curInv, newInv);
+// console.log(updateInventory(curInv, newInv));
+
+console.log(updateInventory([[21, "Bowling Ball"], [2, "Dirty Sock"], [1, "Hair Pin"], [5, "Microphone"]], [[2, "Hair Pin"], [3, "Half-Eaten Apple"], [67, "Bowling Ball"], [7, "Toothpaste"]]))
+//deve retornar um array com comprimento 6.
+
+// updateInventory([[21, "Bowling Ball"], [2, "Dirty Sock"], [1, "Hair Pin"], [5, "Microphone"]], [[2, "Hair Pin"], [3, "Half-Eaten Apple"], [67, "Bowling Ball"], [7, "Toothpaste"]]) 
+//deve retornar [[88, "Bowling Ball"], [2, "Dirty Sock"], [3, "Hair Pin"], [3, "Half-Eaten Apple"], [5, "Microphone"], [7, "Toothpaste"]].
+
+// updateInventory([[21, "Bowling Ball"], [2, "Dirty Sock"], [1, "Hair Pin"], [5, "Microphone"]], []) 
+//deve retornar [[21, "Bowling Ball"], [2, "Dirty Sock"], [1, "Hair Pin"], [5, "Microphone"]].
+
+// updateInventory([], [[2, "Hair Pin"], [3, "Half-Eaten Apple"], [67, "Bowling Ball"], [7, "Toothpaste"]]) 
+//deve retornar [[67, "Bowling Ball"], [2, "Hair Pin"], [3, "Half-Eaten Apple"], [7, "Toothpaste"]].
+
+// updateInventory([[0, "Bowling Ball"], [0, "Dirty Sock"], [0, "Hair Pin"], [0, "Microphone"]], [[1, "Hair Pin"], [1, "Half-Eaten Apple"], [1, "Bowling Ball"], [1, "Toothpaste"]]) 
+//deve retornar [[1, "Bowling Ball"], [0, "Dirty Sock"], [1, "Hair Pin"], [1, "Half-Eaten Apple"], [0, "Microphone"], [1, "Toothpaste"]]
